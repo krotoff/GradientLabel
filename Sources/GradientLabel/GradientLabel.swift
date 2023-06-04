3f7413a0b9edfb113d4afaeb7fb9ce6347baac39
Andrei Krotov <akrotoff95@gmail.com>
2023-06-04 21:17:37 +0300

    Added GradientLabel class

--- Sources/GradientLabel/GradientLabel.swift
+++ Sources/GradientLabel/GradientLabel.swift
@@ -1,6 +1,33 @@
-public struct GradientLabel {
-    public private(set) var text = "Hello, World!"
+import UIKit.UILabel
 
-    public init() {
+public class GradientLabel: UILabel {
+
+    // MARK: - Private properties
+
+    private let gradientImage: UIImage
+
+    // MARK: - Init
+
+    public init(gradientImage: UIImage) {
+        self.gradientImage = gradientImage
+
+        super.init(frame: .zero)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - View lifecycle
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+
+        UIGraphicsBeginImageContext(frame.size)
+        gradientImage.draw(in: bounds)
+        let myGradient = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        textColor = UIColor(patternImage: myGradient!)
     }
 }
